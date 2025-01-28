4671155de1599cb3439e4ab07f765150f4af38be
Jaskirat <jsingh573@my.bcit.ca>
2025-01-27 17:21:24 -0800

    Change the layout of output message

--- Labs/3/modules/utils.js
+++ Labs/3/modules/utils.js
@@ -32,47 +32,26 @@
 // };
 
 
-// function getDate(name, messageTemplate) {
-//     const currentDate = new Date();
-
-//     // Create a date string that includes the required timezone offset for PST
-//     const offset = currentDate.getTimezoneOffset() !== 480 ? -480 : currentDate.getTimezoneOffset(); // PST is UTC-8
-//     const pstDate = new Date(currentDate.getTime() + offset * 60 * 1000);
-
-//     // Custom format the date as: Day Mon DD YYYY HH:MM:SS GMT-0800 (PST)
-//     const formattedDate = pstDate.toDateString() + ' ' + pstDate.toTimeString().split(' ')[0] + ' GMT-0800 (Pacific Standard Time)';
-
-//     // Replace %1 in the message template with the name and append the formatted date
-//     const message = messageTemplate.replace('%1', name);
-//     return `${message} ${formattedDate}`;
-// }
-
-// module.exports = {
-//     getDate
-// };
-
 function getDate(name, messageTemplate) {
-    // Use toLocaleString to format the date directly in PST
     const options = {
         timeZone: 'America/Los_Angeles',
-        weekday: 'short', // "Wed"
-        year: 'numeric', // "2023"
-        month: 'short', // "Sept"
-        day: 'numeric', // "01"
-        hour: '2-digit', // "12"
-        minute: '2-digit', // "52"
-        second: '2-digit', // "14"
+        weekday: 'short',  // "Wed"
+        year: 'numeric',  // "2023"
+        month: 'short',  // "Sept"
+        day: 'numeric',  // "01"
+        hour: '2-digit',  // "12"
+        minute: '2-digit',  // "52"
+        second: '2-digit',  // "14"
         hour12: false,
-        timeZoneName: 'short' // "PST"
+        timeZoneName: 'long'  // "Pacific Standard Time"
     };
     const currentDate = new Date().toLocaleString('en-US', options);
 
     // Replace %1 in the message template with the name and append the formatted date
     const message = messageTemplate.replace('%1', name);
-    return `${message} ${currentDate}`;
+    return `<p style="color:blue;">${message} ${currentDate.replace('Pacific Standard Time', 'GMT-0800 (Pacific Standard Time)')}</p>`;
 }
 
 module.exports = {
     getDate
 };
-
