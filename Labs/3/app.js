be78957c95e461435be9776bbadb4432524fa481
Jaskirat <jsingh573@my.bcit.ca>
2025-02-07 00:25:56 -0800

    Lab4

--- Labs/3/app.js
+++
@@ -1,3 +0,0 @@
-const math = require('./modules/math.js');
-
-console.log(`Hello Jaskirat. The addition is ${math.add(1, 2)}`); 
--- Labs/3/lang/en.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    greeting: "Hello %1, What a beautiful day. Server current date and time is"
-};
