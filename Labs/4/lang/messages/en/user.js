88c1a53e2dcc6fad6eb8b5227e20b5b1a1214c7c
Marc <144364703+marcangelo1@users.noreply.github.com>
2025-02-07 16:57:31 -0800

    change productionendpoint

--- Labs/4/lang/messages/en/user.js
+++ Labs/4/lang/messages/en/user.js
@@ -1,7 +1,8 @@
 const localEndpoint = "http://localhost:8080/api/definitions";
-const productionEndpoint ="https://lab4api.onrender.com/labs/4/api/definitions";
+const productionEndpoint ="https://lab4api.onrender.com/Labs/4/api/definitions";
 const searchedTermMsg = "<strong>Word:</strong> ";
 const searchedDefinitionMsg = "<strong>Definition:</strong> ";
 const failedPostResponseMsg = "Error: No response from server";
 const numRequestsPostMsg = "Request # %NUMREQ (updated on %DATE: Total number of words in the dictionary: %TOTALWORDS)";
 const newEntryPostMsg = "New entry recorded:<br/>%WORD: %DEFINITION";
+
